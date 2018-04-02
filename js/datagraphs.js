6b8762104eb95c5c3f935f3b28d188fc8530a1ec
Dragos Paun <dragos.paun0000@gmail.com>
2018-04-02 18:48:29 +0000

    added data + styles+ graph

--- js/datagraphs.js
+++ js/datagraphs.js
@@ -22,7 +22,8 @@ function show_fuel_type(ndx){
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
         .group(group)
-        .transitionDuration(500)
+        .transitionDuration(1500)
+        .colors(d3.scale.ordinal().range(['#1E2080', '#801E20']))
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Fuel Type")
@@ -39,7 +40,8 @@ function show_type(ndx){
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
         .group(group)
-        .transitionDuration(500)
+        .transitionDuration(1500)
+        .colors(d3.scale.ordinal().range(['#1E2080']))
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Type of car")
@@ -54,63 +56,28 @@ function show_made_in(ndx){
 		dc.pieChart("#made-in")
 		.height(300)
 		.radius(100)
-		.transitionDuration(500)
+		.innerRadius(50)
+		.colors(d3.scale.ordinal().range(['#1E2080', '#CD3033', '#801E20']))
+		.transitionDuration(1500)
 		.dimension(dim)
 		.group(group);
 		
 }
 
+
 function show_reliability(ndx){
-    var price_dim = ndx.dimension(dc.pluck("Price"));
-    
-    var maxPrice = price_dim.bottom(1)[0].Price;
-    var minPrice = price_dim.top(1)[0].Price;
-    console.log(maxPrice);
-    
-    
-    var usaCar = price_dim.group().reduceSum(function (d){
-        if(d.Country == "USA"){
-            return +d.Reliability;
-        }else{
-            return 0;
-        }
-    });
-    var europeCar = price_dim.group().reduceSum(function (d){
-        if(d.Country == "Europe"){
-            return +d.Reliability;
-        }else{
-            return 0;
-        }
-    });
-    var japanCar = price_dim.group().reduceSum(function (d){
-        if(d.Country == "Japan"){
-            return +d.Reliability;
-        }else{
-            return 0;
-        }
-    });
-    
-    
-    var compositeChart = dc.compositeChart("#reliability-price")
-    compositeChart
-        .width(700)
-        .height(200)
-        .dimension(price_dim)
-        .x(d3.scale.ordinal().domain([minPrice,maxPrice]))
-        .xAxisLabel("price")
-        .yAxisLabel("reliability")
-        .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
-        .renderHorizontalGridLines(true)
-        .compose([
-            dc.lineChart(compositeChart)
-                .colors("green")
-                .group(usaCar, "USA"),
-            dc.lineChart(compositeChart)
-                .colors("red")
-                .group(europeCar, "Europe"),
-            dc.lineChart(compositeChart)
-                .colors("blue")
-                .group(japanCar, "Japan")
-            ]);
+	var dim = ndx.dimension(dc.pluck("Reliability"));
+	var group = dim.group();
+	
+		dc.rowChart("#reliability-price")
+		.height(300)
+		.height(250)
+		.dimension(dim)
+		.group(group)
+		.colors(d3.scale.ordinal().range(['#1E2080', '#3033CD', '#CD3033', '#801E20','#1F801E']))
+		.transitionDuration(1500);
+		
    
 }
+
+
--- js/webapp.js
+++ js/webapp.js
@@ -1,9 +1,55 @@
 var i = 0;
-var images = [];
-images[0] = "url(images/road.jpg)";
-images[1] = "url(images/bmw.jpg)";
-images[2] = "url(images/supra.jpg)";
-images[3] = "url(images/mustang.jpg)";
+
+var images = [
+"url(images/road.jpg)",
+"url(images/bmw.jpg)",
+"url(images/supra.jpg)",
+"url(images/mustang.jpg)"
+];
+
+var topImages = [
+"images/toyota.jpg",
+"images/mazda.jpg",
+"images/acura.jpg",
+"images/viper.jpg",
+"images/mclaren.jpg",
+"images/suzuki.jpg",
+"images/ford.jpg",
+"images/honda.jpg",
+"images/lexus.jpg",
+"images/cadillac.jpg"
+];
+
+var topTitle = [
+"Toyota Supra",
+"Mazda RX-7",
+"Acura NSX",
+"Dodge Viper",
+"McLaren F1",
+"Suzuki Samurai",
+"Ford F-150",
+"Honda Civic",
+"Lexus LS",
+"Cadillac Eldorado"
+];
+
+
+var topText = [
+"data/toyota.txt",
+"data/mazda.txt",
+"data/acura.txt",
+"data/viper.txt",
+"data/mclaren.txt",
+"data/suzuki.txt",
+"data/ford.txt",
+"data/honda.txt",
+"data/lexus.txt",
+"data/cadillac.txt"
+];
+
+var clickCount = 0;
+
+
 
 function changeImage(){
 	$(".slider").css({"background-image" : images[i]});
@@ -20,43 +66,38 @@ changeImage();
     $(".info-text").animate({"opacity": "1", "height":"100%", "marginTop" : "-20px"},2000);
      $(".navbar").animate({"opacity": "1"},2000);
 
+  
 
-//function fadePic(){
-//	for (var i = 0; i < images.length ; i++) {
-		//console.log(images[i]);
-//		$(".slider").fadeOut(3000).css({"background-image" : images[i]}).fadeIn(3000);
-//	}
-//};
-
-    
-   
-//    function fadeDivs(){
-  //  	for (var i = 0 ; i< images.length; i++) {
-    //		console.log(i);
-
-      // 		$('.slider').css({'background-image':images[i]}).fadeIn('slow');
-
-    	//}
-    		
-//var i = 0;
-//setInterval(fadeDivs, 3000);
-
-//function fadeDivs() {
-  // for (i=0;i < images.length; i++){
-   	//$(".slider").fadeOut('400', function() {
-   	//	$(this).css('background-image', images[i]).fadeIn(100);
-   	//});                
-   //}
 
 $(".next").click(function(){
-	$("#iconic-car").fadeOut(10).attr('src', 'images/mazda.jpg').fadeIn(2500);
-	$(".car-name").fadeOut(10).text('Mazda RX-7').fadeIn(2500);
-	$(".car-description").load("data/mazda.txt");
-//	$(".car-description").text("The right car, at the wrong time. The last generation RX-7 sported 276 horsepower from a smooth 1.3 liter rotary engine. Sequential twin turbos made predictable power and impressive torque, while the light weight and perfect 50:50 weight distribution made one of the best handling cars of the time, at any price. Unfortunately, sales of all sports cars were down, and the rotary had a hard time meeting emissions and mileage figures, so Mazda canned the entire program.");
+  clickCount++; 
+  if (clickCount >= topImages.length) {
+    clickCount = 0;
+  }
+	$("#iconic-car").animate({"opacity": "0"},10).attr('src', topImages[clickCount]).animate({"opacity": "1"},1500);
+	$(".car-name").fadeOut(10).text(topTitle[clickCount]).fadeIn(1500);
+	$(".car-description").fadeOut(10).load(topText[clickCount]).fadeIn(1500);
+});
+
+$(".prev").click(function(){
+  clickCount--; 
+  if (clickCount < 0) {
+    clickCount = topImages.length - 1;
+  }
+  $("#iconic-car").animate({"opacity": "0"},10).attr('src', topImages[clickCount]).animate({"opacity": "1"},1500);
+  $(".car-name").fadeOut(10).text(topTitle[clickCount]).fadeIn(1500);
+  $(".car-description").fadeOut(10).load("data/mazda.txt").fadeIn(1500);
 });
 
 
-
-
+$.scrollIt({
+  upKey: 38,             // key code to navigate to the next section
+  downKey: 40,           // key code to navigate to the previous section
+  easing: 'linear',      // the easing function for animation
+  scrollTime: 1000,       // how long (in ms) the animation takes
+  activeClass: 'active', // class given to the active nav element
+  onPageChange: null,    // function(pageIndex) that is called when page is changed
+  topOffset: -50           // offste (in px) for fixed top navigation
+});
 
 
